f30e204da6ef2316dd903c5e93ef077ed6c8846c
Benedict475 <ajokub@coventry.ac.uk>
2024-11-20 12:00:14 +0000

    practice

---
+++ my-app/src/components/about.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function About() {
+
+    return(
+        
+<section className="hero is-link">
+  <div className="hero-body">
+    <p className="title">Link hero</p>
+    <p className="subtitle">Link subtitle</p>
+  </div>
+</section>
+    );
+}
---
+++ my-app/src/components/home.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Home() {
+
+    return(
+        
+<section className="hero is-link">
+  <div className="hero-body">
+    <p className="title">Link hero</p>
+    <p className="subtitle">Link subtitle</p>
+  </div>
+</section>
+    );
+}
