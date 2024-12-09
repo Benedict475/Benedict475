bbbc7d52f2475ba5223a46b3a90d575ab76c029d
Benedict475 <ajokub@coventry.ac.uk>
2024-12-09 18:16:15 +0000

    Assignment Update

--- my-app/src/components/moviepage.tsx
+++ my-app/src/components/moviepage.tsx
@@ -1,14 +1,1 @@
-import React from "react";
 
-export default function Movies() {
-
-    return(
-        
-<section className="hero is-large is-info">
-  <div className="hero-body">
-    <p className="title">Large hero</p>
-    <p className="subtitle">Large subtitle</p>
-  </div>
-</section>
-    );
-}
